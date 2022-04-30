f567bffab14a2e82aa837418990f51e87f97476c
hardworkingmission <shakil20131101041@gmail.com>
2022-05-01 03:55:29 +0600

    additem-updated

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const AddItem = () => {
+    const [user, loading, error] = useAuthState(auth);
     const navigate=useNavigate()
-    const [state,setState]=useState({name:'',description:'',price:'',quantity:''})
+    const [state,setState]=useState({name:'',description:'',price:'',supplier:'',quantity:'',email:user?.email})
     const [imgFile,setImgFile]=useState()
     const handleChange=(e)=>{
         setState({...state,[e.target.name]:e.target.value})
@@ -34,9 +37,11 @@ const AddItem = () => {
             <form action="" className='lg:w-[50%] md:w-[70%] border border-2 p-2 rounded' onSubmit={createNewItem}>
                <h1 className='text-center text-2xl my-2 font-bold'>Please Add A Product</h1>
                 <input type="text" className="p-2 mb-2 border-b-2 w-full outline-none rounded"name='name' placeholder='Product Name' required onChange={handleChange} value={state?.name}/><br />
+                <input type="email" className="p-2 mb-2 border-b-2 w-full outline-none rounded" onChange={handleChange} value={user?.email} readOnly/><br />
                 <input type="text" className="p-2 mb-2 border-b-2 w-full outline-none rounded"name="description"placeholder='Specification' required onChange={handleChange} value={state?.description}/><br />
                 <input type="text" className="p-2 mb-2 border-b-2 w-full outline-none rounded"name="price" placeholder='Price' required onChange={handleChange} value={state?.price}/><br/>
                 <input type="text" className="p-2 mb-2 border-b-2 w-full outline-none rounded"name='quantity' placeholder='Quantity' required onChange={handleChange} value={state?.quantity}/><br />
+                <input type="text" className="p-2 mb-2 border-b-2 w-full outline-none rounded"name='supplier' placeholder='Supplier' required onChange={handleChange} value={state?.supplier}/><br />
                 <input type="file" className="p-2 mb-2 border-b-2 w-full outline-none rounded"name="img" required onChange={handleFileChange}/><br />
                 <input type="submit" className='bg-gray-300 w-full p-2 rounded' value="Add" />
             </form>
--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -21,55 +21,55 @@ const MyItems = () => {
     }
     return (
         <div className="w-5/6 mx-auto">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                <div className="overflow-hidden">
-                    <table className="min-w-full">
-                        <thead className="bg-white border-b">
-                        <tr>
-                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Image
-                            </th>
-                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Name
-                            </th>
-                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Specification
-                            </th>
-                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                            Price
-                            </th>
-                            <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
-                            Action
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                myItems?.map(myItem=>(
-                                <tr key={myItem._id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                <td className="px-6 py-4 whitespace-wrap text-sm font-medium text-gray-900"><img src={process.env.PUBLIC_URL+`/images/${myItem?.img}`} alt="" className='h-[50px] w-[50px]' /></td>
-                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap">
-                                {myItem?.name}
-                                </td>
-                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap">
-                                {myItem?.description}
-                                </td>
-                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap">
-                                {myItem?.price}
-                                </td>
-                                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap text-center">
-                                <button className=' bg-red-600 text-white px-2 py-1 rounded ' onClick={()=>deleteItem(myItem._id)}><FontAwesomeIcon icon={faTrash}/></button>
-                                </td>
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden">
+                        <table className="min-w-full">
+                            <thead className="bg-white border-b">
+                            <tr>
+                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Image
+                                </th>
+                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Name
+                                </th>
+                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Specification
+                                </th>
+                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                Price
+                                </th>
+                                <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+                                Action
+                                </th>
                             </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {
+                                    myItems?.map(myItem=>(
+                                    <tr key={myItem._id} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                    <td className="px-6 py-4 whitespace-wrap text-sm font-medium text-gray-900"><img src={process.env.PUBLIC_URL+`/images/${myItem?.img}`} alt="" className='h-[50px] w-[50px]' /></td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap">
+                                    {myItem?.name}
+                                    </td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap">
+                                    {myItem?.description}
+                                    </td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap">
+                                    {myItem?.price}
+                                    </td>
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-wrap text-center">
+                                    <button className=' bg-red-600 text-white px-2 py-1 rounded ' onClick={()=>deleteItem(myItem._id)}><FontAwesomeIcon icon={faTrash}/></button>
+                                    </td>
+                                </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
