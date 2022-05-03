982f989ac65d3cd6e9d4fc29ed65e5abe59d8033
hardworkingmission <shakil20131101041@gmail.com>
2022-05-03 06:30:13 +0600

    spinner-done

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,30 +1,33 @@
+import axios from 'axios';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useProducts from '../../hooks/useProducts/useProducts';
-import CustomSpinner from '../CustomSpinner/CustomSpinner';
 import Product from '../Product/Product'
 
 const Products = () => {
     const [products,setProducts,loading]=useProducts()
-    const navigate=useNavigate()
-   
-    let selectedProducts=null
     const shuffledProducts = products?.sort(() => 0.5 - Math.random());
-    selectedProducts=shuffledProducts.slice(0,6)
-    //console.log(products)
+    const selectedProducts=shuffledProducts.slice(0,6)
+   
+    const navigate=useNavigate()
+    if(loading){
+        return (<div className="flex justify-center items-center my-3">
+                    <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-gray-600" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                </div>)
+    }
     return (
         <div>
             <h1 className='text-center text-4xl font-bold my-3'>Laptops</h1>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3'>
                 {
-                   !selectedProducts?<CustomSpinner/>:selectedProducts?.map(product=><Product key={product._id} product={product}/>)
+                   selectedProducts.map(product=><Product key={product._id} product={product}/>)
                 }
             </div>
             <div className='text-center'>
                 <button className='text-xl font-bold my-3 bg-white p-2 rounded' onClick={()=>navigate('/manageitems')}>Manage Inventories</button>
-
-            </div>
-           
+            </div> 
         </div>
     );
 };
